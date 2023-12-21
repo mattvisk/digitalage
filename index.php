90481758e7e30d2d55833646448885dddf66d337
mattvisk <mattvisk@gmail.com>
2023-12-21 11:04:16 -0500

    content, styling, and imagery

--- index.php
+++ index.php
@@ -11,7 +11,14 @@ $l = isset($_GET['l']) ? $_GET['l'] : null;
 <html>
   <head>
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/tailwindcss@3.0.0/dist/tailwind.min.css" />
     <link rel="stylesheet" href="css/style.css" />   
+    <script>
+      window.addEventListener('scroll', function() {
+        var header = document.querySelector('header');
+        header.classList.toggle('sticky', window.scrollY > 30);
+      });
+    </script>
   </head>
   <body>
     <div class="wrapper">
@@ -20,82 +27,100 @@ $l = isset($_GET['l']) ? $_GET['l'] : null;
     <? /* Header  
     ----------------------------------------*/ ?>
     <header x-data="{ open: false }" :class="{'open': open}">
-      <a class="logo" href="/"><img src="images/logo/da_logo.png" /></a>
-      <nav>
-        <a href="/product">Product</a>
-        <a href="/features">Features</a>
-        <a href="/marketplace">Marketplace</a>
-        <a href="/company">Company</a>
-        <a href="/signup">Sign Up</a>
-      </nav>
-      <button @click="open = !open" class="hamburger">
-        <svg x-show="!open" class="open" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#fff" d="M3 18h18v-2H3v2zM3 13h18v-2H3v2zM3 6v2h18V6H3z"/></svg>
-        <svg x-show="open" class="close" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#fff" d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
-      </button>
+      <div class="inner">
+        <a class="logo" href="/"><img src="images/logo/da_logo.png" /></a>
+        <!-- Nav but add clas when php $p is equal to url -->
+        <nav>
+          <a href="/product" class="<? if($p=='product'){ ?>active<? } ?>">Product</a>
+          <a href="/features" class="<? if($p=='features'){ ?>active<? } ?>">Features</a>
+          <a href="/marketplace" class="<? if($p=='marketplace'){ ?>active<? } ?>">Marketplace</a>
+          <a href="/company" class="<? if($p=='company'){ ?>active<? } ?>">Company</a>
+          <a href="/signup" class="<? if($p=='signup'){ ?>active<? } ?>">Sign Up</a>
+        </nav>
+        <button @click="open = !open" class="hamburger">
+          <svg x-show="!open" class="open" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#fff" d="M3 18h18v-2H3v2zM3 13h18v-2H3v2zM3 6v2h18V6H3z"/></svg>
+          <svg x-show="open" class="close" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#fff" d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
+        </button>
+      </div>
     </header>
+
+
+
+
 
     <? /* Banner
     ----------------------------------------*/ ?>
+    <? if($p==null){ ?>
     <div class="banner">
-    </div>
-
-    <? /* Content 
-    ----------------------------------------*/ ?>
-    <div class="content">
-      <? if($p==null){ ?>
         <h1>The Future of Social Media Starts Now</h1>
-        <p>Digitalage is pioneering a revolutionary social media platform centered around elevating creators, protecting individual rights, and cultivating authentic communities.</p>
-        <p>We are reimagining social technology to put control back into the hands of users - giving you unmatched authority over your personal data and digital footprint. Our platform is designed to provide transparency, ensure content integrity, foster meaningful connections, and enable equitable compensation.</p>
-        <h2>The Digitalage Difference:</h2>
-<p>User-Centric Design: Our interface and algorithms are built to serve user needs - not exploit them. We enable authentic connections without manipulating recommendations or compromising privacy.</p>
-<p>Creator Economy: We offer creators unmatched opportunities to monetize content while retaining rights. Our fair compensation models empower sustainable creator careers.</p>
-<p>Decentralized Architecture: Our decentralized platform powered by blockchain ensures security, transparency and gives users control over their data.</p>
-<p>Content Authenticity: We leverage advanced analytics to authenticate content, counter disinformation, and promote accurate information across the platform.</p>
-<h2>Join the Digitalage Community:</h2>
-<p>We invite creators, consumers, partners and investors to help us reinvent social media technology for good. Sign up today to receive updates and early access.</p>
-<p>Call to Action Button: Join Digitalage Now</p>
-<p>By building an ethical platform focused on transparency and putting principles first, Digitalage aims to restore trust in social systems and online communities. The future starts now - join us.</p>
-      <!-- Content in <p> tags -->
-
-      
-      <? } else if($p=='product'){ ?>
-        <h1>Product</h1>
-
-      <? } else if($p=='features'){ ?>
-        <h1>Features</h1>
-
-      <? } else if($p=='marketplace'){ ?>
-        <h1>Marketplace</h1>
-
-      <? } else if($p=='company'){ ?>
-        <h1>Company</h1>
-
-      <? } else if($p=='signup'){ ?>
-        <h1>Sign Up</h1>
-      <? } ?> 
-
+        <!-- add button -->
+        <button class="btn">
+          Join Waitlist
+        </button>        
     </div>
+    <? } ?>
 
-    </div>
-  </div>
+      <? /* Content 
+      ----------------------------------------*/ ?>
+      <div class="content">
+        <div class="inner">
+          <? if($p==null){ ?>
+            <h1>The Future of Social Media Starts Now</h1>
+            <img alt="Digital painting of a group of young professionals" src="images/ai/digital_painting_a_group_of_young_professionals_dive_ad1e12a5-8c45-47d6-b665-560e376fb41f.png" />
+            <p>Digitalage is pioneering a revolutionary social media platform centered around elevating creators, protecting individual rights, and cultivating authentic communities.</p>
+            <p>We are reimagining social technology to put control back into the hands of users - giving you unmatched authority over your personal data and digital footprint. Our platform is designed to provide transparency, ensure content integrity, foster meaningful connections, and enable equitable compensation.</p>
+            
+            <h2 class="headline-border-top">The Digitalage Difference:</h2>
+            <img alt="Digital painting of a group of young professionals" src="images/ai/illustration_creators_conceptualizing_ideas_on_devic_76ac6f73-b25e-4826-9441-6660f8f54d06.png" />
+            <p><b>User-Centric Design:</b> Our interface and algorithms are built to serve user needs - not exploit them. We enable authentic connections without manipulating recommendations or compromising privacy.</p>
+            <p><b>Creator Economy:</b> We offer creators unmatched opportunities to monetize content while retaining rights. Our fair compensation models empower sustainable creator careers.</p>
+            <p><b>Decentralized Architecture:</b> Our decentralized platform powered by blockchain ensures security, transparency and gives users control over their data.</p>
+            <p><b>Content Authenticity:</b> We leverage advanced analytics to authenticate content, counter disinformation, and promote accurate information across the platform.</p>
 
-  <? /* Footer 
-  ----------------------------------------*/ ?>
-  <div class="footer">
-    <div class="inner">
-      <div class="footer-left">
-        <a href="/product">Product</a>
-        <a href="/features">Features</a>
-        <a href="/marketplace">Marketplace</a>
-        <a href="/company">Company</a>
-        <a href="/signup">Sign Up</a>
+            <h2 class="headline-border-top">Join the Digitalage Community:</h2>
+            <img alt="Digital painting of a group of young professionals" src="images/ai/network_interconnected_pieces_authentic_verified_con_ac462519-f3f7-4d2a-b3db-db60ed4fb060.png" />
+            <p>We invite creators, consumers, partners and investors to help us reinvent social media technology for good. Sign up today to receive updates and early access.</p>
+            <p>Call to Action Button: Join Digitalage Now</p>
+            <p>By building an ethical platform focused on transparency and putting principles first, Digitalage aims to restore trust in social systems and online communities. The future starts now - join us.</p>      
+          <? } else if($p=='product'){ ?>
+            <h1>Product</h1>
+
+          <? } else if($p=='features'){ ?>
+            <h1>Features</h1>
+
+          <? } else if($p=='marketplace'){ ?>
+            <h1>Marketplace</h1>
+
+          <? } else if($p=='company'){ ?>
+            <h1>Company</h1>
+
+          <? } else if($p=='signup'){ ?>
+            <h1>Sign Up</h1>
+          <? } ?> 
+        </div>
       </div>
-      <div class="footer-right">
-        <a href="/privacy">Privacy</a>
-        <a href="/terms">Terms</a>
-        <a href="/contact">Contact</a>
+
+
+
+    <? /* Footer (Ride bottom)
+    ----------------------------------------*/ ?>
+    </div><!-- end inner-wrapper -->
+    </div><!-- end wrapper -->
+    <div class="footer">
+      <div class="inner">
+        <div class="footer-left">
+          <a href="/product">Product</a>
+          <a href="/features">Features</a>
+          <a href="/marketplace">Marketplace</a>
+          <a href="/company">Company</a>
+          <a href="/signup">Sign Up</a>
+        </div>
+        <div class="footer-right">
+          <a href="/privacy">Privacy</a>
+          <a href="/terms">Terms</a>
+          <a href="/contact">Contact</a>
+        </div>
       </div>
     </div>
-  </div>
-</body>
+  </body>
 </html>
